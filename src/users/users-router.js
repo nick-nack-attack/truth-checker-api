db2b92173e48f5921e10eab7a06af4ec715e2a83
Ruben <iruben229@gmail.com>
2020-10-22 20:50:02 -0400

    Implementing a POST method to users endpoint

--- src/users/users-router.js
+++ src/users/users-router.js
@@ -18,5 +18,50 @@ UsersRouter
         .catch(next)
 
     })
+    .post(jsonBodyParser, (req, res, next) => {
+      
+      const { email, password } = req.body;
+
+      const newUser = {
+        email,
+        password,
+      }
+
+      // if a value is missing from the request body then return error
+      for (const [key, value] of Object.entries(newUser)) {
+        if (value === undefined || value === null) {
+          return res
+            .status(400)
+            .json({
+              error: `Missing ${key} in request`
+            })
+        }
+      }
+      
+      // check if email already exists
+      UsersService.getUserEmail(
+        req.app.get('db'), 
+        email
+      )
+        .then(result => {
+          if (!result.length) {
+            res
+              .status(400)
+              .json({
+                error: result
+              })
+          }
+        })
+
+    });
+    
+      // TO-DO:
+      // check if password is valid .validatePassword
+      // hash the password .encryptPassword
+      // insert user .insertUser
+      // api call to random user .fetchRandomUser
+      // return user id, fake user info (201)
+
+      // UsersService.createUser(req.app.get('db'), newUser);
 
     module.exports = UsersRouter;
